26d70e5b2b6ac50122a99347dac7ebd23bf83b1a
akaza-dono <alejoquevedo0598@hotmail.com>
2024-09-27 19:10:46 -0500

    first commit

--- TechnicalTest-API/obj/Debug/net6.0/TechnicalTest-API.AssemblyInfo.cs
+++ TechnicalTest-API/obj/Debug/net6.0/TechnicalTest-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechnicalTest-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78aefef5209222c2300c7035d32c109235dc61e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechnicalTest-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechnicalTest-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
